483b4ac161eefec91b9b876a3844f23341315b66
cpombo <camila.pombo@keycaptureenergy.com>
2023-12-28 22:23:13 +0000

    create method

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,12 @@
 # GIT TASK: Script takes user's input and prints it out
 
-# take user input
-user_text = input('Please enter message to be printed out: ')
 
-# print user input
-print(user_text)
+def print_user_input():
+    # take user input
+    user_text = input('Please enter message to be printed out: ')
+    # print user input
+    print(user_text)
+
+
+# call method to print user's input
+print_user_input()
