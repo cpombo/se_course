4a2214189ce95c1a08d916f412c279be7592ea05
cpombo <camila.pombo@keycaptureenergy.com>
2023-12-28 22:14:38 +0000

    Add comments

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,7 @@
-# GIT TASK
+# GIT TASK: Script takes user's input and prints it out
 
+# take user input
 user_text = input('Please enter message to be printed out: ')
+
+# print user input
 print(user_text)
